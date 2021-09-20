c083c47b3d1bcf47e4fe93c1987a1ec71d443aa7
Kara <palumbokara@yahoo.com>
2021-09-19 21:23:07 -0600

    updating frontend

--- src/FilesUpload.js
+++ src/FilesUpload.js
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export const FilesUpload = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  axios.defaults.headers.common = {
+    "Content-Type": "application/json",
+  };
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    // const s = event.target.value;
+    // const photo = s.split("\\")[2];
+    console.log(event.target.files[0]);
+    setInputValue(event.target.files[0]);
+  };
+
+  const handleUpload = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    const imageFile = inputValue;
+    formData.append("image", imageFile);
+    await axios
+      .post("http://localhost:8000/image", formData)
+      .then((response) => {
+        console.log(response);
+      });
+  };
+
   return (
     <>
       <div className="container">
@@ -8,10 +35,12 @@ export const FilesUpload = () => {
           <form>
             <h3>File Upload</h3>
             <div className="form-group">
-              <input type="file"></input>
+              <input type="file" onChange={handleChange}></input>
             </div>
             <div className="form-group">
-              <button type="submit">Upload</button>
+              <button type="submit" onClick={handleUpload}>
+                Upload
+              </button>
             </div>
           </form>
         </div>
